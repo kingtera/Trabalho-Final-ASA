28df5785f42b937431b073b1d818fe3db5f5173c
EduTera <eduardoteranisi@gmail.com>
2024-10-29 11:04:29 -0300

    removed 'hello world' from main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 from models.user import Base
 from routers.login_route import router as login_route
@@ -10,8 +9,3 @@ Base.metadata.create_all(bind=engine);
 app = FastAPI();
 
 app.include_router(login_route);
-=======
-#teste github
-#testando branch aaaaaa
-print("Hello world");
->>>>>>> 4a2dd93ebe4cc9670a2e56430bc7f3b6fab470da
