21b499c83ad340d12fd78c01ca5c68cf464e1713
EduTera <eduardoteranisi@gmail.com>
2024-10-29 10:58:52 -0300

    include test voos_route

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from models.user import Base
-from routers.login_route import router as login_route
+from routers.login import router as login_route
+from routers.voos import router as voos_route
+#from routers.eticket import router as eticket_route
 from database_login import engine
 
 
@@ -9,3 +11,5 @@ Base.metadata.create_all(bind=engine);
 app = FastAPI();
 
 app.include_router(login_route);
+app.include_router(voos_route);
+#app.include_router(eticket_route);
